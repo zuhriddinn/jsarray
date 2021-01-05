ffdf0bd8ca00941308b35e6239f67e1ba9ad97d8
Zuhriddin <nuriddinovsirojoddin@gmail.com>
2021-01-05 11:28:05 +0500

    versiya1

---
+++ js.js
@@ -0,0 +1,29 @@
+let yoz = [
+
+
+];
+
+
+while (true) {
+
+    let b = prompt('Yoz')
+
+    if (b.split(' ')[0] == 'add,' ) {
+
+        yoz.push(b.split(' ')[1])
+    }
+    else if (b.split(' ')[0] == 'del,' ) {
+
+        for (key in yoz) {
+
+            if (yoz[key] == b.split(' ')[1]) {
+
+                yoz.splice(key, 1)
+            }
+
+        }
+    } else if (b == 'stop') break
+
+
+}
+console.log(yoz);
